f71009d328afe948d1bb0bb1bfb2ca7480970993
sato-koi <sato.koichi.0404@gmail.com>
2019-11-12 23:47:31 +0900

    登録機能作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,21 @@ class ArticlesController < ApplicationController
   def index
     @articles =Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to "/articles"
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
+  
