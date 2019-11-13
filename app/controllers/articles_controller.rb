23a9ad60ee39ae00b27e65238a1309fdd8522a2e
sato-koi <sato.koichi.0404@gmail.com>
2019-11-14 02:17:46 +0900

    更新機能作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,16 @@ class ArticlesController < ApplicationController
     redirect_to "/articles"
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to @article
+  end
+
   private
 
   def article_params
